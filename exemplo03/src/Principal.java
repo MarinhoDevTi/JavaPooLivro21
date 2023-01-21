e25a0789fde084314af10e15a3c3ac494e2f579e
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2023-01-20 23:15:24 -0300

    Update

--- exemplo03/src/Principal.java
+++ exemplo03/src/Principal.java
@@ -1,5 +1,18 @@
+import java.util.Locale;
+
 public class Principal {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
         
+        UnencapsulatedItem monitor = new UnencapsulatedItem( "electronics-012", "17\" SVGA Monitor ", 1, 299.00 );
+
+        monitor.discount = 1.25;  // inválido, o desconto deve ser menor que 100%!
+
+        double price = monitor.getAdjustedTotal();
+        
+
+
     }
+
 }
